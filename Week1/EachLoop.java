ffb52f8f86981906bd3ea140a8d1df6375b39a1b
Minhkl27 <118721048+Minhkl27@users.noreply.github.com>
2024-08-30 06:46:50 +0000

    Remove week1 from main

--- Week1/EachLoop.java
+++
@@ -1,8 +0,0 @@
-public class EachLoop {
-    public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for (String i : cars) {
-            System.out.println(i);
-        }
-    }
-}
