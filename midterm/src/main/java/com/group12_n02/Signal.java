239d87a6a945085e2b90f8f601d9725149f556c8
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-09-11 05:02:06 +0000

    cau truc lai source code bai giua ky

---
+++ midterm/src/main/java/com/group12_n02/Signal.java
@@ -0,0 +1,7 @@
+public interface Signal {
+    
+    double getAmplitude(); /* Biên độ của tín hiệu*/
+    double getFrequency(); /* Tính thường xuyên (tần số) của tín hiệu*/
+    double getPeriod();    /* Chu kỳ của tín hiệu*/
+    double getWavelength(); /* Bước sóng của tín hiệu*/
+}
